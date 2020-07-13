b67ce61ffa2c490ebd6a58bd58243e8ca0c6d50d
Diego448 <diegosg448@gmail.com>
2020-07-13 17:35:22 -0500

    Adding greeting route

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def index():
     message = {'message': 'Welcome to the app!'}
     return json.dumps(message)
 
+@app.route('/greeting')
+def greeting():
+    message = {'message': 'Hello user'}
+    return json.dumps(message)
+
+@app.route('/greeting/<name>')
+def gretting_by_name(name):
+    message = {'message': 'Hello ' + name}
+    return json.dumps(message)
+
 if __name__ == "__main__":
     app.run(port=5000)
